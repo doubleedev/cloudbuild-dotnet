1a744588e72573e76f97da5de8b6c09f6f08b3e0
Kevin Trasler <KTRASLER@jaguarlandrover.com>
2022-06-28 03:42:23 +0100

    Third Commit

--- GCPWebAPI/Startup.cs
+++ GCPWebAPI/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,11 +26,6 @@ namespace GCPWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "spo_api", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
